520ef49192aa0dbcc7087532dbde277c31ffeadb
aitkennhsu <aitkenhsu@gmail.com>
2013-09-05 17:57:15 +0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitTraining::Application.config.secret_key_base = 'dfd0201fea08051219e651eb51a3e9578660a59a9f4bf00a6b0d574344dcf88261244e943b28f088dac2e11cb0919e76a5b1054d4fc1a2315411f23e7460d5c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GitTraining::Application.config.session_store :cookie_store, key: '_GitTraining_session'
